fcc7e25a37b946bb3d88ffba9f93b2d22f79e693
AurelienOrchestra <aurelien.ticot@orchestranetworks.com>
2017-11-30 22:43:28 +0100

    add logs on index.js

--- index.js
+++ index.js
@@ -2,26 +2,45 @@
 const _ = require('lodash');
 const args = require('yargs').argv;
 
+// Getting debug and verbose argument from command line
+const debug = args.debug;
+const verbose = args.verbose;
+
+// Set up loggers
+require('./logger')(debug, verbose);
+// Get the logger
+const logger = require('winston').loggers.get('App');
+
 // Get the mandatory license page url argument from command line
 const licensePageURL = args.url || process.env.LICENSE_PAGE_URL;
 if (licensePageURL == null) {
-    throw new Error('The url of the license page is mandatory!');
+    const err = new Error('The url of the license page is mandatory!');
+    logger.error(err);
+    throw err;
 }
+logger.log('debug', `License page URL: ${licensePageURL}`);
 
 // Get the mandatory slackbot token argument from command line
 const slackBotToken = args.token || process.env.SLACKBOT_TOKEN;
 if (slackBotToken == null) {
-    throw new Error('The token of the slackbot is mandatory!');
+    const err = new Error('The token of the slackbot is mandatory!');
+    logger.error(err);
+    throw err;
 }
+logger.log('debug', `Slackbot token: ${slackBotToken}`);
 
 // Get the port argument from command line
 const port = args.port || process.env.PORT || 3000;
+logger.log('debug', `port: ${port}`);
 
 // Initialize the EBX license provider
+logger.info('Launch the EBX license provider');
 const EBXLicensesProvider = require('./provider')(licensePageURL);
 
 // Define the REST API
+logger.info('Launch the REST API');
 require('./api')(EBXLicensesProvider, port);
 
 // Define the slackbot
+logger.info('Launch the slackbot');
 require('./bot')(EBXLicensesProvider, slackBotToken);
