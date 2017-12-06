5805082310586672983d713f6de361ac032e0aa2
AurelienOrchestra <aurelien.ticot@orchestranetworks.com>
2017-12-06 11:35:02 +0100

    document the command line options to use --help

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
 // Dependencies
 const path = require('path');
 const _ = require('lodash');
-const args = require('yargs').argv;
 const ErrorsFactory = require('errors-factory');
 const errors = new ErrorsFactory(require('./errors.json'));
+const yargs = require('yargs');
 
-// Getting debug and verbose argument from command line
-const debug = args.debug || false;
-const verbose = args.verbose || false;
+// Define command line options
+yargs.option('url', {
+    alias: 'u',
+    describe: 'Provide the url of the license page to parse',
+    demandOption : true,
+    type: 'string'
+});
+yargs.option('token', {
+    alias: 't',
+    describe: 'Provide the Slack bot token',
+    demandOption : true,
+    type: 'string'
+});
+yargs.option('port', {
+    alias: 'p',
+    describe: 'Provide the port the server will listen to',
+    type: 'number',
+    default: 3000
+});
+yargs.option('logDir', {
+    describe: 'Provide the path of the log directory',
+    type: 'string',
+    default: './log'
+});
+yargs.option('verbose', {
+    alias: 'v',
+    describe: 'Enable verbose logs',
+    type: 'boolean',
+    default: false
+});
+yargs.option('debug', {
+    describe: 'Enable debug logs',
+    type: 'boolean',
+    default: false
+});
 
-// Getting the log directory from conmmand line
-const logDir = args.logDir || `./log`;
+const args = yargs.argv;
+
+// Getting the log options from command line
+const logDir = args.logDir;
+const verbose = args.verbose;
+const debug = args.debug;
 
 // Set up loggers
 require('./logger')(logDir, debug, verbose);
@@ -18,7 +54,7 @@ require('./logger')(logDir, debug, verbose);
 const logger = require('winston').loggers.get('App');
 
 // Get the mandatory license page url argument from command line
-const licensePageURL = args.url || process.env.LICENSE_PAGE_URL;
+const licensePageURL = process.env.LICENSE_PAGE_URL || args.url;
 if (licensePageURL == null) {
     const err = errors.ERR_APP_001;
     logger.error(err);
@@ -27,7 +63,7 @@ if (licensePageURL == null) {
 logger.log('debug', `License page URL: ${licensePageURL}`);
 
 // Get the mandatory slackbot token argument from command line
-const slackBotToken = args.token || process.env.SLACKBOT_TOKEN;
+const slackBotToken = process.env.SLACKBOT_TOKEN || args.token;
 if (slackBotToken == null) {
     const err = errors.ERR_APP_002;
     logger.error(err);
@@ -36,7 +72,7 @@ if (slackBotToken == null) {
 logger.log('debug', `Slackbot token: ${slackBotToken}`);
 
 // Get the port argument from command line
-const port = args.port || process.env.PORT || 3000;
+const port = process.env.PORT || args.port;
 logger.log('debug', `port: ${port}`);
 
 // Initialize the EBX license provider
